e2b67b12c8f16c86f242ad2f438da88026f445e2
HernadiB <hernadi.barnabas@njit.hu>
2022-04-09 16:52:47 +0200

    #2 OP-Fix 2. users_migration, username-email unique

--- Web/UserInterface/Laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ Web/UserInterface/Laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('full_name', 255);
-            $table->string('username', 255);
-            $table->string('email', 255);
+            $table->string('username', 255)->unique();
+            $table->string('email', 255)->unique();
             $table->string('password');
             $table->string('role', 255);
             $table->string('profile_picture')->nullable();
