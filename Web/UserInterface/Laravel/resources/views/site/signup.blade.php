05d17c52cedfceb0fcaf04c403991defecb9ad39
HernadiB <hernadi.barnabas@njit.hu>
2022-04-11 17:00:51 +0200

    OP-3 HTML CSS Bejelentkezés, regisztráció oldalak

--- Web/UserInterface/Laravel/resources/views/site/signup.blade.php
+++ Web/UserInterface/Laravel/resources/views/site/signup.blade.php
@@ -12,31 +12,31 @@
         <hr>
         <form action="">
             <div class="nev">
-                <label for="nev">Teljes név</label>
-                <input type="text" name="nev">
+                <label for="nev" class="form-label">Teljes név</label>
+                <input type="text" name="nev" class="form-control" id="input" placeholder="Alfréd Mihály">
             </div>
             <div class="felhnev">
-                <label for="felhnev">Felhasználónév</label>
-                <input type="text" name="nev">
+                <label for="felhnev" class="form-label">Felhasználónév</label>
+                <input type="text" name="nev" class="form-control" id="input" placeholder="alfred.mihaly">
             </div>
             <div class="email">
-                <label for="email">E-mail cím</label>
-                <input type="text" name="email" required>
+                <label for="email" class="form-label">E-mail cím</label>
+                <input type="text" name="email" required class="form-control" id="input" placeholder="alfred.mihaly@example.com">
                 <span></span>
             </div>
             <div class="passwd">
-                <label for="passwd">Jelszó</label>
-                <input type="password" name="passwd" required>
+                <label for="passwd" class="form-label">Jelszó</label>
+                <input type="password" name="passwd" required class="form-control" id="input" placeholder="*************">
                 <span></span>
             </div>
             <div class="passwd">
-                <label for="passwdscnd">Jelszó megerősítése</label>
-                <input type="password" name="passwdscnd" required>
+                <label for="passwdscnd" class="form-label">Jelszó megerősítése</label>
+                <input type="password" name="passwdscnd" required class="form-control" id="input" placeholder="*************">
                 <span></span>
             </div>
-            <button class="btn">Bejelentkezés</button>
+            <button class="btn">Regisztráció</button>
             <div class="signup-link">
-                <p>Már van fiókom <a href="{{route("site.login")}}">Bejelentkezés</a></p>
+                <p>Már van fiókom <a href="{{route("site.login")}}">Bejelentkezek</a></p>
             </div>
         </form>
     </div>
