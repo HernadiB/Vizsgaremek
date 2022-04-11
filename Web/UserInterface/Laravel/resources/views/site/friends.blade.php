d2ea287bcd719402088f6a6524f08a4e10520d88
HernadiB <hernadi.barnabas@njit.hu>
2022-04-11 09:25:14 +0200

    OP-7 HTML CSS Barátaim oldal

--- Web/UserInterface/Laravel/resources/views/site/friends.blade.php
+++ Web/UserInterface/Laravel/resources/views/site/friends.blade.php
@@ -5,4 +5,41 @@
 @include('site.nav')
 @section('content')
 
+    <table class="barataim">
+        <h3 class="text-center" style="margin-top: 100pt;">Barátaim</h3>
+        <thead>
+            <tr>
+                <th scope="col">Helyezés</th>
+                <th scope="col">Név</th>
+                <th scope="col">Pontszám</th>
+                <th scope="col">Barát törlése</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td data-label="Helyezés">#1</td>
+                <td data-label="Név">#1 feladat megnevezése</td>
+                <td data-label="Pontszám">#1 feladat leírása</td>
+                <td data-label="Barát törlése">
+                    <button class="btn btn-danger">Törlés</button>
+                </td>
+            </tr>
+            <tr>
+                <td data-label="Helyezés">#2</td>
+                <td data-label="Név">#2 feladat megnevezése</td>
+                <td data-label="Pontszám">#2 feladat leírása</td>
+                <td data-label="Barát törlése">
+                    <button class="btn btn-danger">Törlés</button>
+                </td>
+            </tr>
+            <tr>
+                <td data-label="Helyezés">#3</td>
+                <td data-label="Név">#3 feladat megnevezése</td>
+                <td data-label="Pontszám">#3 feladat leírása</td>
+                <td data-label="Barát törlése">
+                    <button class="btn btn-danger">Törlés</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 @endsection
