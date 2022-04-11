56b4d5feb0ea59d3e7182e6b9a655466bd253333
HernadiB <hernadi.barnabas@njit.hu>
2022-04-11 11:48:03 +0200

    OP-9 HTML CSS Taskok oldal

--- Web/UserInterface/Laravel/resources/views/site/mytasks.blade.php
+++ Web/UserInterface/Laravel/resources/views/site/mytasks.blade.php
@@ -5,4 +5,42 @@
 @include('site.nav')
 @section('content')
 
+
+    <table class="feladataim">
+        <h3 class="text-center" style="margin-top: 100pt;">Aktuális feladataim</h3>
+        <thead>
+        <tr>
+            <th scope="col">Feladat sorszáma</th>
+            <th scope="col">Feladat megnevezése</th>
+            <th scope="col">Feladat</th>
+            <th scope="col">Státusz</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td data-label="Feladat sorszáma">#1</td>
+            <td data-label="Feladat megnevezése">#1 feladat megnevezése</td>
+            <td data-label="Feladat">
+                <button class="btn btn-dark">Megtekint</button>
+            </td>
+            <td data-label="Státusz">&#10004</td>
+        </tr>
+        <tr>
+            <td data-label="Feladat sorszáma">#2</td>
+            <td data-label="Feladat megnevezése">#2 feladat megnevezése</td>
+            <td data-label="Feladat">
+                <button class="btn btn-dark">Megtekint</button>
+            </td>
+            <td data-label="Státusz">&#x274C;</td>
+        </tr>
+        <tr>
+            <td data-label="Feladat sorszáma">#3</td>
+            <td data-label="Feladat megnevezése">#3 feladat megnevezése</td>
+            <td data-label="Feladat">
+                <button class="btn btn-dark">Megtekint</button>
+            </td>
+            <td data-label="Státusz">&#10004</td>
+        </tr>
+        </tbody>
+    </table>
 @endsection
