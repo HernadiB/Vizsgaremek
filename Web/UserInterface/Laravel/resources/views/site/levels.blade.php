99dba3e77772c37d924cee4e22fd99a442fb9a6f
BilibocB2 <biliboc.bence@njit.hu>
2022-04-12 14:37:37 +0200

    OP-9.1 HTML CSS Szintek-feladatok oldal

--- Web/UserInterface/Laravel/resources/views/site/levels.blade.php
+++ Web/UserInterface/Laravel/resources/views/site/levels.blade.php
@@ -4,5 +4,71 @@
 @endsection
 @include('site.nav')
 @section('content')
-
+    <table class="szintek">
+        <h3 id="cimsor" class="text-center" >Szintek - feladatok</h3>
+        <thead>
+        <tr>
+            <th scope="col">Szint neve</th>
+            <th scope="col">Feladat megnevezése</th>
+            <th scope="col">Feladat leírása</th>
+            <th scope="col">Kapható pontok</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td data-label="Helyezés">Tisztek</td>
+            <td data-label="Név">#1 feladat megnevezése</td>
+            <td data-label="Leírás">#1 feladat leírása</td>
+            <td data-label="Pontszám">32</td>
+        </tr>
+        <tr>
+            <td data-label="Helyezés">Tisztek</td>
+            <td data-label="Név">#2 feladat megnevezése</td>
+            <td data-label="Leírás">#2 feladat leírása</td>
+            <td data-label="Pontszám">32</td>
+        </tr>
+        <tr>
+            <td data-label="Helyezés">Tisztek</td>
+            <td data-label="Név">#3 feladat megnevezése</td>
+            <td data-label="Pontszám">#3 feladat leírása</td>
+            <td data-label="Leírás">32</td>
+        </tr>
+        <tr>
+            <td data-label="Helyezés">Altiszt</td>
+            <td data-label="Név">#1 feladat megnevezése</td>
+            <td data-label="Pontszám">#1 feladat leírása</td>
+            <td data-label="Leírás">32</td>
+        </tr>
+        <tr>
+            <td data-label="Helyezés">Altiszt</td>
+            <td data-label="Név">#2 feladat megnevezése</td>
+            <td data-label="Pontszám">#2 feladat leírása</td>
+            <td data-label="Leírás">32</td>
+        </tr>
+        <tr>
+            <td data-label="Helyezés">Altiszt</td>
+            <td data-label="Név">#3 feladat megnevezése</td>
+            <td data-label="Pontszám">#3 feladat leírása</td>
+            <td data-label="Leírás">32</td>
+        </tr>
+        <tr>
+            <td data-label="Helyezés">Zászlós</td>
+            <td data-label="Név">#1 feladat megnevezése</td>
+            <td data-label="Pontszám">#1 feladat leírása</td>
+            <td data-label="Leírás">32</td>
+        </tr>
+        <tr>
+            <td data-label="Helyezés">Zászlós</td>
+            <td data-label="Név">#2 feladat megnevezése</td>
+            <td data-label="Pontszám">#2 feladat leírása</td>
+            <td data-label="Leírás">32</td>
+        </tr>
+        <tr>
+            <td data-label="Helyezés">Zászlós</td>
+            <td data-label="Név">#3 feladat megnevezése</td>
+            <td data-label="Pontszám">#3 feladat leírása</td>
+            <td data-label="Leírás">32</td>
+        </tr>
+        </tbody>
+    </table>
 @endsection
