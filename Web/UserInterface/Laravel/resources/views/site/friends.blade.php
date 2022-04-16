481403b560c59ba22d203d296f6ba2dc32499474
HernadiB <hernadi.barnabas@njit.hu>
2022-04-16 16:36:28 +0200

    #9 OP-Fix 9. Barát oldalon adott barát megtekintése gomb + kereses

--- Web/UserInterface/Laravel/resources/views/site/friends.blade.php
+++ Web/UserInterface/Laravel/resources/views/site/friends.blade.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="{{asset("css/friends_style.css")}}">
 @extends('layouts.main')
 @section('title')
     {{$title}}
@@ -10,8 +11,61 @@
     @if(session("error"))
         <div class="alert alert-danger">{{session("error")}}</div>
     @endif
+    <div class="baratkereses">
+        <div class="form-section">
+            <form action="" method="" role="form" class="form-inline">
+                <h4 id="h4">Barátok keresése</h4>
+                <div class="nev row">
+                    <label for="nev" class="col-lg-4 col-form-label">Felhasználó neve</label>
+                    <div class="col-lg-8">
+                        <input type="text" name="nev" class="form-control input" placeholder="Alfréd Mihály">
+                    </div>
+                </div>
+                <div class="table">
+                    <table class="barattabla">
+                        <thead>
+                        <tr>
+                            <th scope="col">Név</th>
+                            <th scope="col">Profil</th>
+                            <th scope="col">Bejelöl</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td data-label="Név">Biliboc Bence</td>
+                            <td data-label="Profil">
+                                <button class="btn btn-dark">Megtekint</button>
+                            </td>
+                            <td data-label="Bejelöl">
+                                <button class="btn btn-success">Bejelöl</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td data-label="Név">Hernádi Barnabás</td>
+                            <td data-label="Profil">
+                                <button class="btn btn-dark">Megtekint</button>
+                            </td>
+                            <td data-label="Bejelöl">
+                                <button class="btn btn-success">Bejelöl</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td data-label="Név">Nyári Roland</td>
+                            <td data-label="Profil">
+                                <button class="btn btn-dark">Megtekint</button>
+                            </td>
+                            <td data-label="Bejelöl">
+                                <button class="btn btn-success">Bejelöl</button>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </form>
+        </div>
+    </div>
     @if(count($friends) != 0)
-    <h3 class="text-center" id="cimsor">Barátaim</h3>
+    <h3 class="text-center">Barátaim</h3>
     <table class="barataim">
         <thead>
             <tr>
@@ -19,6 +73,7 @@
                 <th scope="col">Név</th>
                 <th scope="col">Pontszám</th>
                 <th scope="col">Barát törlése</th>
+                <th scope="col">Profil</th>
             </tr>
         </thead>
         <tbody>
@@ -32,6 +87,9 @@
                     {!! Form::open(['route' => 'friendDelete', 'method' => 'post']) !!}
                         <button name="friendID" value="{{$friend->id}}" class="btn btn-danger">Törlés</button>
                     {!! Form::close() !!}
+                </td>
+                <td data-label="Profil">
+                    <button class="btn btn-dark">Megtekint</button>
                 </td>
             </tr>
             @php $i++ @endphp
