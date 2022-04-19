241e1daa79e446bfd3d54326b5292d1d2a77a60f
HernadiB <hernadi.barnabas@njit.hu>
2022-04-19 22:51:52 +0200

    OP-24 C# Kép felvétel, tárolás

---
+++ C#/AdminInterface/Algorithms/Base64.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace AdminInterface.Algorithms
+{
+    public class Base64
+    {
+        public static string Encode(string filePath)
+        {
+            Bitmap bitmap = new Bitmap((string)filePath);
+            MemoryStream ms = new MemoryStream();
+            bitmap.Save(ms, ImageFormat.Jpeg);
+            byte[] byteImage = ms.ToArray();
+            string base64 = Convert.ToBase64String(byteImage);
+            return base64;
+        }
+        public static BitmapImage Decode(string base64)
+        {
+            byte[] binaryData = Convert.FromBase64String(base64);
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.StreamSource = new MemoryStream(binaryData);
+            image.EndInit();
+            return image;
+        }
+    }
+}
