e56bd44d07162ea92a82493574b3f261b1313e35
BilibocB2 <biliboc.bence@njit.hu>
2022-04-25 21:22:11 +0200

    #21 OP-Fix 20.JS adatok felvitele modalba Szintek feladatok oldalon

--- Web/UserInterface/Laravel/public/js/modal.js
+++ Web/UserInterface/Laravel/public/js/modal.js
@@ -10,21 +10,24 @@ function ShowProfile(user)
     let hr = document.createElement('hr');
     let birthdate = document.createElement('h4');
     birthdate.classList.add('text-dark');
+    let gender = document.createElement('h4');
+    gender.classList.add('text-dark');
     let email = document.createElement('h4');
     email.classList.add('text-dark');
     let level = document.createElement('h4');
     level.classList.add('text-dark');
     let score = document.createElement('h4');
     score.classList.add('text-dark');
-    profilePicture.src = "./../" + user.ProfilePicture;
+    profilePicture.src = "./../" + user.ProfilePicture ;
 
     name.innerText = user.FullName + "\n" + "  (" +  user.Username + ")";
     teamRole.innerText = user.Team + " (" + ((user.Role == "Felhasználó") ? "Csapattag" : "Vezető") + ")";
     birthdate.innerText = 'Született: ' + user.Birthdate;
+    gender.innerText = 'Neme: ' + user.Gender;
     email.innerText = 'E-mail: ' + user.Email;
     level.innerText = 'Szint: ' + (user.Level ?? "-");
     score.innerText = 'Pontszám: ' + (user.Score ?? "-");
-    modalBody.append(profilePicture, name, teamRole, hr, birthdate, email, level, score);
+    modalBody.append(profilePicture, name, teamRole, hr, birthdate, gender, email, level, score);
 
     if (user.Team == null)
     {
@@ -33,13 +36,24 @@ function ShowProfile(user)
 
 }
 
-function ShowTaskImage(image)
+function ShowTask(task)
 {
+    console.log(task)
     let modalBody = document.querySelector('div#exampleModal').querySelector('div.modal-body');
-    modalBody.innerHTML = "";
     let taskPicture = document.createElement('img');
+    modalBody.innerHTML = "";
+    let taskDescription = document.createElement('h4');
+    taskDescription.innerText = 'Feladat leírása: ' + task.description;
+    let taskScore = document.createElement('h4');
+    taskScore.innerText = 'Feladat pontszáma: ' + task.score;
+
+    taskDescription.classList.add('text-dark');
+    taskScore.classList.add('text-dark');
+
     taskPicture.classList.add('img-fluid', 'mx-auto', 'd-block');
     taskPicture.style.width = "400px";
-    taskPicture.src = "./../" + image;
-    modalBody.append(taskPicture);
+    taskPicture.src = "./../" + task.image;
+    modalBody.append(taskPicture, taskDescription, taskScore);
+
+
 }
--- Web/UserInterface/Laravel/resources/views/site/levels.blade.php
+++ Web/UserInterface/Laravel/resources/views/site/levels.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.main')
-@section('title')
-    {{$title}}
-@endsection
+@section('title', $title)
+
 @section('js_css')
     <script src="{{asset('js/modal.js')}}"></script>
     <script src="{{asset('js/fetch.js')}}"></script>
@@ -20,9 +19,7 @@
         <tr>
             <th scope="col">Szint neve</th>
             <th scope="col">Feladat megnevezése</th>
-            <th scope="col">Feladat leírása</th>
-            <th scope="col">Feladat képe</th>
-            <th scope="col">Kapható pontok</th>
+            <th scope="col">Feladat Paraméterei</th>
         </tr>
         </thead>
         <tbody class="table-content">
@@ -30,11 +27,9 @@
                 <tr @if(session('taskID') == $task->id) class="myTask" @endif>
                     <td id="levelname" data-label="Szint">{{\App\Models\Level::where('id', $task->level_id)->first()->name}}</td>
                     <td id="taskname" data-label="Név">{{$task->name}}</td>
-                    <td id="description" data-label="Leírás">{{$task->description}}</td>
                     <td id="image" data-label="Kép">
-                        <button class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="ShowTaskImage('{{$task->image}}')">Megtekint</button>
+                        <button class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="ShowTask({{$task}})">Megtekint</button>
                     </td>
-                    <td id="score" data-label="Pontszám">{{$task->score}}</td>
                 </tr>
             @endforeach
         </tbody>
