d2e0cca45806c001bf0a1eb4ddb389c98c49ff3f
BilibocB2 <biliboc.bence@njit.hu>
2022-04-25 18:14:36 +0200

    OP-31 Plusz funkciók Beállítások oldal

--- Web/UserInterface/Laravel/resources/views/site/settings.blade.php
+++ Web/UserInterface/Laravel/resources/views/site/settings.blade.php
@@ -6,5 +6,29 @@
 <link rel="stylesheet" href="{{asset("css/setting_style.css")}}">
 @endsection
 @section('content')
-
+    <div class="form-section">
+        <h2>Beállítások</h2>
+        <hr>
+        <form action="" method="post">
+            @csrf
+            <div class="toggler">
+                <div class="form-check form-switch">
+                    <label class="form-check-label" for="flexSwitchCheckDefault">Smooth Háttér Ki/Be</label>
+                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                </div>
+                <div class="form-check form-switch">
+                    <label class="form-check-label" for="flexSwitchCheckDefault">Dark mode Ki/Be</label>
+                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                </div>
+                <div class="form-check form-switch">
+                    <label class="form-check-label" for="flexSwitchCheckDefault">Időjárás jelentő a főoldalon</label>
+                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                </div>
+                <div class="form-check form-switch">
+                    <label class="form-check-label" for="flexSwitchCheckDefault">NEU áramfejlesztő lekapcsolása</label>
+                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                </div>
+            </div>
+        </form>
+    </div>
 @endsection
