34f090c727fb3a317fea503429bd06496a066818
BilibocB2 <biliboc.bence@njit.hu>
2022-04-29 09:11:16 +0200

    #31 OP-Fix HTML CSS Csapatom oldal kártyák rendezése, leírás formázása

--- Web/UserInterface/Laravel/resources/views/components/teamMembers.blade.php
+++ Web/UserInterface/Laravel/resources/views/components/teamMembers.blade.php
@@ -4,7 +4,7 @@
         <div class="col-md-6 col-sm-12">
             <h3 class="title">Vezető</h3>
             <a class="leader" href="#" onclick="GetUserByID({{$team->Leader->id}})" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                <div class="card border-0 w-50">
+                <div class="card border-0 w-50 w-md-50">
                    <div class="card-header text-center px-0 py-0">
                        <img src="{{asset($team->Leader->profile_picture)}}" class="card-img-top img-fluid" alt="{{$team->Leader->username}} profilképe">
                    </div>
@@ -14,17 +14,17 @@
                 </div>
             </a>
         </div>
-        <div class="col-md-6 col-sm-12 my-auto">
+        <div class="col-md-6 col-sm-12 my-auto teamDescription">
             <p class="leaderDecription ">{{$team->description}}</p>
         </div>
     </div>
     <h3 class="title">Csapattagok</h3>
     <div class="row">
         @foreach($team->Members as $teamMember)
-            <div class="col-lg-3 col-md-4 col-sm-6 my-3">
+            <div class="col-lg-4 col-md-4 col-sm-6 my-3">
                 <a class="teamMember" href="#" onclick="GetUserByID({{$teamMember->id}})" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     <div class="card border-0 w-75 teamMemberCard">
-                        <div class="card-header px-0 py-0">
+                        <div class="card-header px-0 py-0 ">
                             <img src="{{asset($teamMember->profile_picture)}}" class="card-img-top img-fluid" alt="{{$teamMember->username}} profilképe">
                         </div>
                         <div class="card-body">
