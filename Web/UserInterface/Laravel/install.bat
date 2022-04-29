7ef4c5067dd8cb8dc3f7ee11de643aa4c7986a84
BilibocB2 <biliboc.bence@njit.hu>
2022-04-29 11:25:42 +0200

    #32 OP-Fix 32. install.bat megírásra a projekt beüzemeléséhez

--- Web/UserInterface/Laravel/install.bat
+++ Web/UserInterface/Laravel/install.bat
@@ -1,4 +1,9 @@
 copy .env.example .env
+docker-compose build
 docker-compose up -d
 docker-compose exec php composer install
 docker-compose exec php php artisan key:generate
+docker-compose exec php php artisan migrate:fresh
+docker-compose exec php php artisan db:seed
+docker-compose exec php mkdir -p public/images 
+docker-compose exec php php artisan storage:link
